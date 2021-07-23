27b1651eb5d0c3ec41632b5414d5d3a7c0c636c6
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2021-07-23 06:20:53 -0600

    Update TInterpreter.h

--- TInterpreter.h
+++ TInterpreter.h
@@ -1,6 +1,6 @@
 /****************************************************
 Written by Tomas de Camino Beck.
-tomas@funcostarica.org
+tomas.decamino@gmail.com
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
